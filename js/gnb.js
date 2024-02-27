5d3cc148953dc6accf5f0c29dfa9a2aefc28071d
Lee Inji <lij8016@gmail.com>
2024-02-27 16:54:15 +0900

    🔨 Imprement gnb sub menu dropdown

--- js/gnb.js
+++ js/gnb.js
@@ -5,6 +5,7 @@ const gateBtn = $('.toggle-gate')
 const langBtn = $('.lang-btn')
 const searchBtn = $('.search-btn')
 const closeBtn = $('.search-close-btn')
+
 const selectBtn = $('.select-btn')
 const selectBox = $('.select-box')
 const selectDropdownIcon = $('.select-ico')
@@ -29,6 +30,34 @@ searchBtn.on('click', function () {
 // search 모달창 닫기
 closeBtn.on('click', function () {
   $(searchModal).removeClass('on')
+})
+
+// nav 상단 메뉴 마우스 올렸을 때
+$('.gnb-list').on('mouseenter', function () {
+  let $sub = $(this).find('.sub')
+  let headerHeight = 70
+  let subOffsetHeight = $sub.outerHeight()
+  $('.snb').css('height', `${subOffsetHeight + headerHeight}px`)
+  $sub.addClass('on')
+  $(this).find('.gnb-list-title').addClass('on')
+
+  $('.sub').on('mouseenter', function () {
+    $(this).addClass('on')
+    $('.snb').css('height', `${subOffsetHeight + headerHeight}px`)
+  })
+})
+
+// nav 상단 메뉴 마우스 나갔을 때
+$('.gnb-list').on('mouseleave', function () {
+  $('.snb').css('height', '70px')
+  let $sub = $(this).find('.sub')
+  $(this).find('.gnb-list-title').removeClass('on')
+  $sub.removeClass('on')
+
+  $('.sub').on('mouseleave', function () {
+    $(this).removeClass('on')
+    $('.snb').css('height', '70px')
+  })
 })
 
 // 지점 선택 버튼
