1d18aebd638b4b8b7001b2a9290c8e99a985ce03
Lee Inji <lij8016@gmail.com>
2024-03-08 00:50:03 +0900

    🔨 Imprement mobile footer dropdown  menu

---
+++ mobile/js/footer.js
@@ -0,0 +1,17 @@
+const familySite = $('.family-site')
+const familySiteList = $('.family-site-content')
+const siteBtn = $('.family-site-btn')
+const topBtn = $('.aside-top-btn')
+
+// family site dropdown 버튼
+siteBtn.on('click', function () {
+  familySite.toggleClass('on')
+})
+
+// 스크롤 최상단 이동 top 버튼
+topBtn.on('click', function (e) {
+  e.preventDefault()
+  $('html, body').animate({
+    scrollTop: '0',
+  })
+})
